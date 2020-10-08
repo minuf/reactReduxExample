f232e69d8f998577c37e3cde130b119f1cd18213
jorge-msi <minuel.jc@gmail.com>
2020-10-08 05:07:27 +0200

    Added some folders, structuring app, added redux and configured for simple user login actions.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { store } from './_helpers';
+
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
 
